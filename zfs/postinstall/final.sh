febd6ea5eb18276e36b54af4d9a40a13246dd82c
Artem Korolev <artkor@live.ru>
2019-03-13 00:32:27 +0200

    final.sh

--- zfs/postinstall/final.sh
+++ zfs/postinstall/final.sh
@@ -12,3 +12,5 @@ passwd aur
 pacman -S linux-headers
 sudo -u aur sh ./aur-sripts/install-soft.sh
 pacman -S linux-hardened linux-hardened-headers
+pacman -S gmrun xterm openbox xorg-xinit git vim opera xscreensaver xorg-setxkbmap xorg-server
+#pacman -S virtualbox-guest-dkms virtualbox-guest-utils
